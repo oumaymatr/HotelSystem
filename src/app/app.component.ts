aa8e9369a4e4b6457876c1163d571d628e72d447
oumaymatr <trabelsioumaima@hotmail.com>
2024-11-16 17:21:32 +0100

    create register page+route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NgZorroAntdModule } from './NgZorroAntdModules';
 import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgZorroAntdModule, ReactiveFormsModule],
+  imports: [RouterModule, RouterOutlet, NgZorroAntdModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
