27f90db543b16493f00883542d4d86a11999c19d
oumaymatr <trabelsioumaima@hotmail.com>
2024-11-17 21:22:08 +0100

    creating post room page with Reactive Form

--- src/app/modules/admin/admin.component.ts
+++ src/app/modules/admin/admin.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
+import { NgZorroAntdModule } from '../../NgZorroAntdModules';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [AdminRoutingModule],
+  imports: [AdminRoutingModule, NgZorroAntdModule, ReactiveFormsModule, FormsModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,9 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzLayoutComponent } from 'ng-zorro-antd/layout';
+import { PostRoomComponent } from './components/post-room/post-room.component';
+import { NgZorroAntdModule } from '../../NgZorroAntdModules';
 
 
 @NgModule({
-  imports: [CommonModule, AdminRoutingModule],
+    imports: [
+      PostRoomComponent,
+      CommonModule,
+      AdminRoutingModule,
+      NzLayoutComponent,
+      NgZorroAntdModule,
+      ReactiveFormsModule,
+      AdminRoutingModule,
+      NzFormModule
+    ]
 })
 export class AdminModule {}
